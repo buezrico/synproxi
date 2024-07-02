b43dea64e4a36f4358227ac0752201e9db30c5cd
buezrico <ricobuez@gmail.com>
2024-07-02 14:18:59 +0000

    list of courses

--- app/components/Hero.tsx
+++ app/components/Hero.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import React from "react";
-import { Bounce } from "react-awesome-reveal";
+import { Bounce, Slide } from "react-awesome-reveal";
 import { FiMessageCircle } from "react-icons/fi";
 import { IoIosArrowForward } from "react-icons/io";
 import { Typewriter } from "react-simple-typewriter";
@@ -11,13 +11,15 @@ import Image from "next/image";
 export default function Hero() {
   return (
     <main className="min-h-screen w-full justify-center items-center flex flex-col-reverse md:flex-row gap-4 mt-32 md:mt-20 pt-10 md:pt-0">
-      <div className="h-full w-full sm:h-[70%] sm:w-[70%] md:h-full md:w-full flex items-center justify-center md:items-end">
-        <Image
-          src={girl_with_laptop}
-          alt="girl with laptop"
-          className="h-[70%] w-[70%]"
-        />
-      </div>
+      <Slide>
+        <div className="h-full w-full sm:h-[70%] sm:w-[70%] md:h-full md:w-full flex items-center justify-center md:items-end">
+          <Image
+            src={girl_with_laptop}
+            alt="girl with laptop"
+            className="h-[70%] w-[70%]"
+          />
+        </div>
+      </Slide>
 
       <div className="w-full flex flex-col gap-4">
         <Bounce>
@@ -43,20 +45,20 @@ export default function Hero() {
           />
         </p>
         <div className="mt-8 flex flex-col justify-center lg:flex-row gap-4 w-full">
-          {/* <Bounce delay={1500}> */}
-          <button className="group px-8 py-4 transition-all text-slate-50 bg-blue-600 hover:bg-blue-800 flex items-center justify-center gap-4 rounded-full shadow-lg hover:shadow-sm w-full lg:w-fit">
-            <p className="font-medium">Find Course</p>
+          <Bounce>
+            <button className="group px-8 py-4 transition-all text-slate-50 bg-blue-600 hover:bg-blue-800 flex items-center justify-center gap-4 rounded-full shadow-lg hover:shadow-sm w-full lg:w-fit">
+              <p className="font-medium">Find Course</p>
 
-            <IoIosArrowForward size={24} />
-          </button>
-          {/* </Bounce> */}
-          {/* <Bounce delay={2500}> */}
-          <button className="group px-8 py-4 transition-all text-blue-600 bg-transparent border border-blue-600 hover:border-blue-800 flex items-center justify-center gap-4 rounded-full shadow-lg hover:shadow-sm duration-300 w-full lg:w-fit">
-            <p className="font-medium">Get in Touch</p>
+              <IoIosArrowForward size={24} />
+            </button>
+          </Bounce>
+          <Bounce>
+            <button className="group px-8 py-4 transition-all text-blue-600 bg-transparent border border-blue-600 hover:border-blue-800 flex items-center justify-center gap-4 rounded-full shadow-lg hover:shadow-sm duration-300 w-full lg:w-fit">
+              <p className="font-medium">Get in Touch</p>
 
-            <FiMessageCircle size={22} />
-          </button>
-          {/* </Bounce> */}
+              <FiMessageCircle size={22} />
+            </button>
+          </Bounce>
         </div>
       </div>
     </main>
