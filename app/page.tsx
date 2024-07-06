357ac6f336c13c55f0b6db2012030264d2ef6e69
buezrico <ricobuez@gmail.com>
2024-07-06 08:45:21 +0000

    increased gap between sections in homepage

--- app/page.tsx
+++ app/page.tsx
@@ -5,11 +5,10 @@ import About from "@/app/components/About";
 
 export default function Home() {
   return (
-    <div className="flex flex-col gap-8 pb-20">
+    <div className="flex flex-col gap-8 md:gap-16 lg:gap-20 pb-20">
       <Hero />
       <Courses />
       <About />
-
       <Countup />
     </div>
   );
